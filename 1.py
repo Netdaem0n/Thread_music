a761291ec81fcfb3ae08ef3f43b825f6d12943e0
netdaem0n <bakalets@gmail.com>
2022-04-01 09:39:10 +1100

    update var 2

--- 1.py
+++ 1.py
@@ -2,25 +2,27 @@ from playsound import playsound
 import os
 import sys
 from threading import Thread
-a = ""
+
+commanda = ""
+
 
 def play_sound():
     playsound(os.getcwd() + '\\music\\Halcyon.mp3')
 
+
 def hi_men():
     while True:
         print("hello")
-        a = input()
-        if a == "exit":
+        global commanda
+        commanda = input()
+        if commanda == "exit":
             sys.exit()
-
+        variant2 = lambda x: sys.exit() if x.lower() == "bye" else print("Not this time!\n")
+        variant2(commanda)
 
 
 t1 = Thread(target=play_sound, args=(), daemon=True)
 t2 = Thread(target=hi_men, args=())
 
-
 t1.start()
 t2.start()
-t1.join()
-t2.join()
